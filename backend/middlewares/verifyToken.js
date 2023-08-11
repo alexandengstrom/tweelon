63ad91a566473e04fa079ecf73e09695fadeeecc
Alexander Engström <aleen361@student.liu.se>
2023-08-11 15:12:09 +0200

    adding some comments

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -1,27 +1,41 @@
+// Required dependencies
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
+// Load environment variables
 dotenv.config();
 
 const verifyToken = (req, res, next) => {
+    // Extract the 'Authorization' header from the request
     const authHeader = req.header("Authorization");
     
+    // If there's no 'Authorization' header, deny access
     if (!authHeader) return res.status(401).send("Access denied");
 
+    // Extract the JWT token from the 'Authorization' header (removing "Bearer" prefix)
     const token = authHeader.replace("Bearer ", "");
 
+    // If the token isn't present, deny access
     if (!token) return res.status(401).send("Access denied");
 
     try {
+        // Verify the JWT token using the secret key
         const verified = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+        
+        // Attach the verified user's payload to the request object for subsequent middleware
         req.user = verified;
+        
+        // Proceed to the next middleware function
         next();
     } catch(err) {
+        // If the error is specifically a JWT error, send an "Invalid token" response
         if (err instanceof jwt.JsonWebTokenError) {
             return res.status(401).send("Invalid token");
         }
+        // For all other errors, send a "Bad request" response
         return res.status(400).send("Bad request");
     }
 }
 
+// Export the middleware for use in other parts of the application
 module.exports = verifyToken;
--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -1,35 +1,45 @@
+// Import the mongoose module for schema definition and model creation
 const mongoose = require('mongoose');
 
+// Define the schema for notifications
 const notificationSchema = new mongoose.Schema({
+    // Reference to the user receiving the notification
     user: { 
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
+    // Reference to the post associated with the notification (if applicable)
     post: { 
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Post'
     },
+    // Type of notification (like, follow, reply)
     type: { 
         type: String,
         enum: ["like", "follow", "reply"],
         required: true
     },
+    // User who triggered the notification (e.g., someone who liked a post)
     triggeredBy: { 
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
+    // Boolean flag to indicate if the notification has been read
     isRead: {
         type: Boolean,
         default: false
     },
+    // Timestamp indicating when the notification was generated
     date: { 
         type: Date,
         default: () => Date.now()
     }
 });
 
+// Create a mongoose model based on the notification schema
 const Notification = mongoose.model('Notification', notificationSchema);
 
+// Export the Notification model for use in other parts of the application
 module.exports = Notification;
